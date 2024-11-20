05cf0379d86a9a27e570b7643b024c7bdea57c74
Matthias Luebken <matthias.luebken@gmail.com>
2024-11-20 13:58:38 +0100

    subnet creation

--- vms/aws/aws.tf
+++ vms/aws/aws.tf
@@ -2,17 +2,60 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "dev_vm_1" {
-  ami           = "ami-0084a47cc718c111a" # Ubuntu Server 24.04 'ubuntu'
+# aws ec2 describe-vpcs --filters "Name=tag:Name,Values=MainVPC"
+resource "aws_vpc" "main" {
+  tags                 = { Name = "MainVPC" }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "main" {
+  tags   = { Name = "MainInternetGateway" }
+  vpc_id = aws_vpc.main.id
+
+}
+
+# aws ec2 describe-subnets --filters "Name=tag:Name,Values=PublicSubnet"
+# aws ec2 describe-subnets --filters "Name=tag:Name,Values=PublicSubnet" | jq -r .Subnets[0].SubnetId
+resource "aws_subnet" "public_subnet" {
+  tags                    = { Name = "PublicSubnet" }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
+}
+
+# aws ec2 describe-route-tables --filters "Name=association.subnet-id,Values="
+resource "aws_route_table" "main" {
+  tags   = { Name = "MainRouteTable" }
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.main.id
+}
+
+# aws ec2 describe-instances --filters "Name=tag:Name,Values=AppVM1"
+resource "aws_instance" "app_vm_1" {
+  tags          = { Name = "AppVM1" }
+  ami           = "ami-0084a47cc718c111a" # Ubuntu Server 24.04 user:'ubuntu'
   instance_type = "t2.micro"
   key_name      = "id_rsa_dev"
+  subnet_id     = aws_subnet.public_subnet.id
 
-  # Security Group
   vpc_security_group_ids = [aws_security_group.dev_vm_sg.id]
 }
 
+# aws ec2 describe-security-groups
 resource "aws_security_group" "dev_vm_sg" {
   name_prefix = "dev-vm-sg-"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     description = "Allow SSH"
--- vms/aws/readme.md
+++ vms/aws/readme.md
@@ -1,11 +1,14 @@
 # Setting up VMs with AWS
 
 ```sh
+# Configure & Test AWS access
+aws sts get-caller-identity
+
 ## Install
 ```sh
 terraform init / plan / apply
 
-terraform refresh
-PUBLIC_IP=$(terraform show -json | jq -r .values.root_module.resources[0].values.public_ip)
+
+PUBLIC_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=DevVM1" | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
 ssh -i ~/.ssh/id_rsa_dev ubuntu@$PUBLIC_IP
 ```
