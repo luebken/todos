fffad6f620255e9b0443f4cb3a44ca02b6cb1cf8
Matthias Luebken <matthias.luebken@gmail.com>
2024-12-17 13:08:11 +0100

    add ingress rule to allow PostgreSQL traffic on port 5432

--- vms/aws/aws.tf
+++ vms/aws/aws.tf
@@ -195,6 +195,14 @@ resource "aws_security_group" "data_vm_sg" {
   }
 
   ingress {
+    description = "Allow PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow All ICMP - IPv4"
     from_port   = -1 # all ICMP types
     to_port     = -1
